f15a2256edcb74d9a56c42944d652f97528f4f3c
Archish <archishthakkar@gmail.com>
2023-10-04 17:22:07 +0530

    :art: getter setter

--- app/main.go
+++ app/main.go
@@ -3,10 +3,26 @@ package main
 import (
 	stuff "example/project/mypackage"
 	"fmt"
+	"log"
 	"reflect"
 )
 
 func main()  {
 	fmt.Println("Hello ",stuff.Name)
 	fmt.Println("Hello ", reflect.TypeOf(stuff.IntArrToStrArr([]int{1,2,3,4})))
+
+	date := stuff.Date{}
+	err := date.SetMonth(12)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = date.SetDay(1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = date.SetYear(2010)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("1st Day : %d/%d/%d", date.Day(), date.Month(), date.Year())
 }
